9908784c61f8c1988704cb2f59cbddde92c737e4
jennifersd <jennifersduenas@gmail.com>
2017-03-01 00:35:41 +0100

    changes blog, create views base and post list, add assest folder and style blog and general

--- mysite/settings.py
+++ mysite/settings.py
@@ -106,4 +106,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Static files (CSS, JavaScript, Images) on ass
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
 
