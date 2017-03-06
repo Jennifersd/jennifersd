02a5daa0a8188bd299f5eb685044642b2b63ccc6
jennifersd <jennifersduenas@gmail.com>
2017-03-07 00:09:33 +0100

    add button delete post and system registration - create, delete and publish post or draft

--- mysite/settings.py
+++ mysite/settings.py
@@ -99,6 +99,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Login redirect
+LOGIN_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
