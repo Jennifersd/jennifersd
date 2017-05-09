2e1a6d2cd6d5db57024c16e8b531115e23692c57
jennifersd <jennifersduenas@gmail.com>
2017-05-10 00:37:11 +0200

    changes design list categories and change media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
 // Menu 
+window.onload = function(){ 
 
-(function() {
-    var burger = document.querySelector(".burger-container"),
-        header = document.querySelector(".header-container");
+	(function() {
+	    var burger = document.querySelector(".burger-container"),
+	        header = document.querySelector(".header-container");
+	
+	    burger.onclick = function() {
+	        header.classList.toggle("menu-opened");
+	    };
+	})();
+};
 
-    burger.onclick = function() {
-        header.classList.toggle("menu-opened");
-    };
-})();
+//
+$(document).ready(function(){
+	(function() {
+		$('.blocks-container > h3').click(function() {
+			    $( ".blocks-container > ul" ).slideToggle( "slow" );
+		});
+
+	})();
+
+});
