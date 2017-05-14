a4171ade4ddb5c2cb3f47c114b68cc922ada226d
jennifersd <jennifersduenas@gmail.com>
2017-05-14 15:47:16 +0200

    design blog post and changes menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,20 @@ window.onload = function(){
 
 	(function() {
 	    var burger = document.querySelector(".burger-container"),
+	    	header_content = document.querySelector(".header-wrap"),
 	        header = document.querySelector(".header-container");
+	    
 	
 	    burger.onclick = function() {
 	        header.classList.toggle("menu-opened");
+	        header_content.classList.toggle("header-opened");
 	    };
 	})();
+	
+
 };
 
-//
+// Desplegable categorias
 $(document).ready(function(){
 	(function() {
 		$('.blocks-container > h3').click(function() {
@@ -19,5 +24,26 @@ $(document).ready(function(){
 		});
 
 	})();
+	
+	(function() {
+	  function fixDiv() {
+	    var $cache = $('#social-fixed');
+	    if ($(window).scrollTop() > 750){
+	      
+	      $cache.addClass("fixed-nav");
 
+		} else {
+			$cache.removeClass("fixed-nav");
+		}
+	     
+	      
+	  }
+	  $(window).scroll(fixDiv);
+	  fixDiv();
+	})();
+
+	
 });
+
+
+
