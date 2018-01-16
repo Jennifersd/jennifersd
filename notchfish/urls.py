7df52bdeace5eceb02de8837d8f70553891c2686
Jennifer Sánchez Dueñas <jennifersanchezduenas@MacBook-de-Jennifer.local>
2018-01-17 00:56:17 +0100

    changes notchfish

--- notchfish/urls.py
+++ notchfish/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
+from django.conf import settings
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.home, name='home', app_name='notchfish'),
 ]
