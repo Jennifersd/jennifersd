353469ee1f4f96c900a166747a4b41d9f91a20dc
Jennifer Sánchez Dueñas <jennifersanchezduenas@MacBook-de-Jennifer.local>
2018-01-19 22:42:39 +0100

    changes notchfish

--- mysite/hosts.py
+++ mysite/hosts.py
@@ -1,11 +1,11 @@
-from django.conf import settings
+#from django.conf import settings
 from django_hosts import patterns, host
 
-host_patterns = patterns('',
-    host(r'www', settings.ROOT_URLCONF, name='www'),  # redirect 
-    host(r'prueba', settings.ROOT_URLCONF, name='prueba'),
+host_patterns = patterns('path.to',
+#    host(r'www', settings.ROOT_URLCONF, name='www'),  # redirect 
+#    host(r'prueba', settings.ROOT_URLCONF, name='prueba'),
     host(r'notchfish', 'notchfish.urls', name='notchfish'), #subdomain
-    host(r'(?!www).*', 'mysite.hostsconf.urls', name='wildcard'),
+#    host(r'(?!www).*', 'mysite.hostsconf.urls', name='wildcard'),
 )
 
 '''
