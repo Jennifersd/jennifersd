1ecfefebad7fc7de012e03c746eb73fc29a72e29
Jennifer Sánchez Dueñas <jennifersanchezduenas@MacBook-de-Jennifer.local>
2018-01-30 20:00:55 +0100

    changes hosts

--- mysite/hosts.py
+++ mysite/hosts.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django_hosts import patterns, host
 
 host_patterns = patterns('',
-    host(r'www', settings.ROOT_URLCONF, name='www'),  # redirect 
+	host(r'www', settings.ROOT_URLCONF, name='www'),  # redirect 
     host(r'notchfish', 'notchfish.urls', name='notchfish'),
 #    host(r'notchfish', 'notchfish.urls', name='notchfish'), #subdomain
 #    host(r'(?!www).*', 'mysite.hostsconf.urls', name='wildcard'),
