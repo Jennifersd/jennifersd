ec7789375214658851d56d27e0c4b8f89e3bc2ce
Jennifer Sánchez Dueñas <jennifersanchezduenas@MacBook-de-Jennifer.local>
2018-01-30 19:40:59 +0100

    changes hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'zh4vyn%(k1%wa5&!#5-dnm9cn)&%x8y0qobt8z9@h$*f_d1=cq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['*', 'jennifersd.com' , 'www.jennifersd.com'  , 'notchfish.jennifersd.com' , 'prueba.jennifersd.com'  , '127.0.0.1' , '178.62.125.104' , '10.211.55.4']
+ALLOWED_HOSTS = ['jennifersd.com' , 'www.jennifersd.com'  , 'notchfish.jennifersd.com' , 'prueba.jennifersd.com'  , '127.0.0.1' , '178.62.125.104' , '10.211.55.4']
 # Application definition
 
 INSTALLED_APPS = (
